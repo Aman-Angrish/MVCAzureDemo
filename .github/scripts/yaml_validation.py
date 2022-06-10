8f0d9bd1d7249180e85902cf74f5690147f6e972
Aman Angrish <amanangrish@gmail.com>
2022-06-10 12:05:41 +0530

    Create yaml_validation.py

---
+++ .github/scripts/yaml_validation.py
@@ -0,0 +1,22 @@
+import sys
+import yaml
+from deepdiff import DeepDiff
+
+def yaml_as_dict(my_file):
+  my_dict = {}
+  with open(my_file, 'r') as fp:
+    docs = yaml.safe_load_all(fp)
+    for doc in docs:
+      for key, value in doc.items():
+        my_dict[key] = value
+  return my_dict
+
+if __name__ == '__main__':
+  mainYamlFile = yaml_as_dict(sys.argv[1])
+  featureYamlFile = yaml_as_dict(sys.argv[2])
+  ddiff = DeepDiff(mainYamlFile, featureYamlFile, ignore_order=True)
+  if ddiff:
+    print(ddiff)
+    sys.exit(1)
+  else:
+    print('Success')
